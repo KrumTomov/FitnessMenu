645382ee8fd648fbce229306c91bb52eec4ad3d2
krumtomov <krumtomov@gmail.com>
2024-07-21 19:01:43 +0300

    Initial

---
+++ src/test/java/bg/softuni/FitnessMenu/FitnessMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.FitnessMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

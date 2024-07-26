32934d7d362a074dfb0490fe761462d332e1cebe
krumtomov <krumtomov@gmail.com>
2024-07-26 18:46:30 +0300

    Service logic

---
+++ src/main/java/bg/softuni/FitnessMenu/init/DatabaseInitializer.java
@@ -0,0 +1,27 @@
+package bg.softuni.FitnessMenu.init;
+
+
+import bg.softuni.FitnessMenu.service.CategoryService;
+import bg.softuni.FitnessMenu.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final RoleService roleService;
+    private final CategoryService categoryService;
+
+    @Autowired
+    public DatabaseInitializer(RoleService roleService, CategoryService categoryService) {
+        this.roleService = roleService;
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        roleService.seedRoles();
+        categoryService.seedCategories();
+    }
+}
---
+++ src/main/java/bg/softuni/FitnessMenu/scheduler/InactiveUsersCleanupScheduler.java
@@ -0,0 +1,22 @@
+package bg.softuni.FitnessMenu.scheduler;
+
+
+import bg.softuni.FitnessMenu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InactiveUsersCleanupScheduler {
+
+    private final UserService userService;
+
+    @Autowired
+    public InactiveUsersCleanupScheduler(UserService userService) {
+        this.userService = userService;
+    }
+    @Scheduled(cron = "0 0 0 * * *")
+    public void cleanupInactiveUsers() {
+        userService.deactivateInactiveUsers();
+    }
+}
